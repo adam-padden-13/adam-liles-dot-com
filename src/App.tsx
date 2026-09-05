a7fff8e7cd10f29cc591ca5051d91a34baecc365
adam-padden-13 <paddenadam@gmail.com>
2026-09-05 10:56:11 -0400

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,38 @@
-import { Button } from "@/components/ui/button"
+import AdamLogo1 from "@/assets/adam-logo-1.svg?react"
+import AdamGtr from "@/assets/adam-gtr.jpg"
 
 export function App() {
   return (
-    <div className="flex min-h-svh p-6">
-      <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
-        <div>
-          <h1 className="font-medium">Project ready!</h1>
-          <p>You may now add components and start building.</p>
-          <p>We&apos;ve already added the button component for you.</p>
-          <Button className="mt-2">Button</Button>
-        </div>
-        <div className="font-mono text-xs text-muted-foreground">
-          (Press <kbd>d</kbd> to toggle dark mode)
-        </div>
-      </div>
-    </div>
+    <main className="mx-auto flex min-h-svh w-full max-w-280 flex-col gap-6 p-6">
+      <header className="flex justify-center">
+        <AdamLogo1 className="block h-auto w-full max-w-180 text-primary" />
+      </header>
+      <section className="flex flex-row gap-4">
+        <img
+          src={AdamGtr}
+          alt="Adam Liles Photographer: Christoper Jarvis"
+          className="max-w-70"
+        />
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+          <br />
+          <br />
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </section>
+    </main>
   )
 }
 
